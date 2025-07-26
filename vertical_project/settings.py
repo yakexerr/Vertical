7eede6849ead3bd180d319c601701f3c99dc16c4
yakexerr <senya.evseev.05@mail.ru>
2025-07-26 20:25:26 +0300

    refactor: add gunicorn & whitenoise

--- vertical_project/settings.py
+++ vertical_project/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'vertical_project.urls'
@@ -109,3 +110,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / "staticfiles"
