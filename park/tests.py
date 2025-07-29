1d6b2f958f911a55db0b9e0d608fa986eb4b0f1b
yakexerr <senya.evseev.05@mail.ru>
2025-07-29 20:06:59 +0300

    feat: Add test and CI workflow

--- park/tests.py
+++ park/tests.py
@@ -1,5 +1,6 @@
 import pytest
 from rest_framework.test import APIClient
+from .models import Park, Entertainment
 
 pytestmark = pytest.mark.django_db
 
@@ -11,3 +12,18 @@ def test_parks_api_returns_200():
     response = client.get('/api/v1/parks/')
 
     assert response.status_code == 200
+
+def test_api_create_park_without_title_fails():
+    client = APIClient()
+    park_data = {
+        'city': "Tmz",
+        'address': "Pushkina",
+        "description": "Описание",
+        "is_work": "true",
+        "schedule": "11:00 - 21:00"
+    }
+
+    response = client.post('/api/v1/parks/', data=park_data)
+    assert response.status_code == 400
+    assert Park.objects.count() == 0
+
