9aa5e2e5885776b61b31b84197b635384f481d37
yakexerr <senya.evseev.05@mail.ru>
2025-07-29 07:15:44 +0300

    ci: Add GitHub Actions workflow for testing

--- park/tests.py
+++ park/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import pytest
+from rest_framework.test import APIClient
 
-# Create your tests here.
+pytestmark = pytest.mark.django_db
+
+def test_parks_api_returns_200():
+    '''
+    Проверит, что эндпоинт парков возвразает статус 200 ОК.
+    '''
+    client = APIClient()
+    response = client.get('/api/v1/parks/')
+
+    assert response.status_code == 200
